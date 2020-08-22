7dd513ba948b80fd91d87de6a33759ec55585726
Corinyi <leejimmy0407@gmail.com>
2020-08-23 02:34:53 +0900

    hazaebal

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var express = require('express')
 var fs = require('fs')    // 파일 로드를 위한 모듈
 var app = express()
- 
+
 app.locals.pretty = true
 app.set('views', './view_file')
 app.set('view engine', 'pug')
 app.listen(3000, () => {
   console.log("Server has been started")
 })
- 
+
 // 최상위 라우트로 접속 시 /hello로 리다이렉트 
 app.get("/", (req, res) => {
   res.redirect('/hello')
 })
- 
+
 app.get("/hello", (req, res) => {
   // html 파일 로드
   fs.readFile('index.html', (error, data) => {
