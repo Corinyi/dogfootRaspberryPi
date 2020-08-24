31cba07f8487e5ff16ab90f254b9f5dfefb558a7
Corinyi <leejimmy0407@gmail.com>
2020-08-24 16:25:51 +0900

    dugeundugeun

--- change.js
+++
@@ -1,9 +0,0 @@
-
-// header라는 id를 가지고 있는 <h1> 태그를 찾아 변수에 저장합니다.
-var element = document.getElementById("h1");
-
-// 요소의 콘텐츠를 변경합니다.
-element.innerText = "Hello World!";
-
-// 요소의 콘텐츠를 출력합니다.
-document.write(element.innerText);
